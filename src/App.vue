40d4f015ecd9e6d3158f890ec8449bf78d616048
Zijie Wu <noojeff@gmail.com>
2019-01-13 13:10:05 -0500

    Added info

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
     <div id="app">
-        <drop-zone></drop-zone>
+        <drop-zone v-on:fileUploaded="fileUploaded"></drop-zone>
+        <div id="info">File uploaded: <br> {{ info }}</div>
     </div>
 </template>
 
@@ -11,13 +12,33 @@
         name: 'app',
         components: {
             DropZone
+        },
+        data: function () {
+            return {
+                info: []
+            };
+        },
+        methods: {
+            fileUploaded: function (files) {
+                let list = [];
+
+                for (let i = 0; i < files.length; i++) {
+                    list.push(files[i].name);
+                }
+
+                this.info = list;
+            }
         }
     }
 </script>
 
-<style>
+<style lang="scss">
     #app {
         width: 240px;
         height: 150px;
+
+        #info {
+            margin: 20px;
+        }
     }
 </style>
